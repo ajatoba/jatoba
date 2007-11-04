7dc8d5bc25a65305d4ba06cd3d8d46a6bd446ed1
ajatoba <ajatoba@5f52b151-bf3b-0410-8f2c-f7afc7a904d2>
2007-11-04 21:34:21 +0000

    Versao com novas funcionalidades - Indicacao de Post

---
+++ trunk/blog/WEB-INF/src/com/vanguarda/blog/action/RecomendationAction.java
@@ -0,0 +1,67 @@
+/*
+ * Created on 14/03/2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.vanguarda.blog.action;
+
+import javax.servlet.http.*;
+
+import org.apache.struts.action.*;
+import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.util.MessageResources;
+
+import com.vanguarda.blog.BlogManager;
+import com.vanguarda.blog.form.RecomendationForm;
+import com.vanguarda.blog.util.SendMail;
+
+
+public class RecomendationAction extends DispatchAction {
+
+	
+	public ActionForward sendRecomendation(ActionMapping mapping,
+			ActionForm form, HttpServletRequest req, HttpServletResponse resp)
+			throws Exception {
+
+		RecomendationForm form2 = (RecomendationForm) form;
+		
+		MessageResources messageResources = null;
+		
+		try {
+			
+			messageResources = getResources(req);
+			
+			SendMail mail = new SendMail();
+			
+			String smtpServer = (String) BlogManager.getInstance().getProperties().get("smtp");
+			
+			String message = messageResources.getMessage("recomendation_message") ;
+			message = message.replaceAll("<TO_NAME>", form2.getToName());
+			message = message.replaceAll("<FROM_NAME>", form2.getFromName());
+			message = message.replaceAll("<TO_MAIL>", form2.getToMail());
+			message = message.replaceAll("<FROM_MAIL>", form2.getFromMail());
+			message = message.replaceAll("<POST_ID>",String.valueOf(form2.getPostId()));
+						
+			mail.setSmtpServer(smtpServer);
+			mail.setContentType((String)BlogManager.getInstance().getProperties().get("mail_format"));
+			
+			mail.setFrom(form2.getFromMail());
+			mail.setTo(form2.getToMail());
+			
+			mail.setSubject(messageResources.getMessage("recomendation_subject"));
+			mail.setMessage(message);
+			
+			mail.send();
+			
+			req.setAttribute("sucesso",messageResources.getMessage("recomendation_success"));
+									
+		} catch (Exception e) {
+			req.setAttribute("error", e.getMessage());
+		}
+						
+		return mapping.findForward("addRecomendation");
+
+	}
+	
+}

e8a91ae67d5ab9e920aeeddb19686a904d208fac
ajatoba <ajatoba@5f52b151-bf3b-0410-8f2c-f7afc7a904d2>
2007-11-04 21:34:21 +0000

    Versao com novas funcionalidades - Indicacao de Post

---
+++ blog/WEB-INF/src/com/vanguarda/blog/form/RecomendationForm.java
@@ -0,0 +1,46 @@
+package com.vanguarda.blog.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class RecomendationForm extends ActionForm{
+
+	private int postId;
+	private String toMail;
+	private String toName;
+	private String fromMail;
+	private String fromName;
+	
+	public int getPostId() {
+		return postId;
+	}
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+	public String getToMail() {
+		return toMail;
+	}
+	public void setToMail(String toMail) {
+		this.toMail = toMail;
+	}
+	public String getToName() {
+		return toName;
+	}
+	public void setToName(String toName) {
+		this.toName = toName;
+	}
+	public String getFromMail() {
+		return fromMail;
+	}
+	public void setFromMail(String fromMail) {
+		this.fromMail = fromMail;
+	}
+	public String getFromName() {
+		return fromName;
+	}
+	public void setFromName(String fromName) {
+		this.fromName = fromName;
+	}
+	
+	
+	
+}
